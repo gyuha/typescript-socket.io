12c05c1fbeb0ef66cdc91beb2f47e7e3b28cabee
gyuha <nicegyuha@gmail.com>
2018-10-31 19:22:41 +0900

    채팅 룸 생성

--- public/socket.js
+++ public/socket.js
@@ -1,10 +1,15 @@
-var socket;
+var socket = null;
 
 $(function() {
   connectRoom();
 });
 
 function connectRoom() {
+  if (socket !== null) {
+    console.log('disconnect');
+    socket.close();
+  }
+
   var channel = $('#channel').val();
   socket = io('http://127.0.0.1:4040', {
     query: 'ch=' + channel,
